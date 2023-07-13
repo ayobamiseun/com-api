d6c910d0a2da3cb5ff680613f4841e01139c42cc
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-07-13 13:40:35 +0100

    mvp

--- app.js
+++ app.js
@@ -1,50 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 require("dotenv").config();
-const https = require('https')
-const http = require('http')
+const https = require("https");
+const http = require("http");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
 
 var app = express();
 
-const port = process.env.port || 3000
+const port = process.env.port || 3000;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-
-http.createServer(app).listen(port)
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`app running fine on ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/users.js')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/users', async (req, res) => {
+  const user = new User(req.body)
+  try {
+      await user.save()
+      const token = await user.generateAuthToken()
+      res.status(201).send({user, token})
+  } catch (error) {
+      res.status(400).send(error)
+  }
+  })
 
 module.exports = router;
