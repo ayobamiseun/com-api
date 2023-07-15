723737c3c29573444cc1ac225059ac2880ba39a0
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-07-15 09:45:39 +0100

    more sorting params

--- controllers/item.js
+++ controllers/item.js
@@ -11,7 +11,9 @@ const createItem = async(req,res) => {
 }
 
 const getItem = async (req, res) => {
-   const item  = await Item.find({})
+   const item  = await Item.find({ price: { $gt: 10 } })
+   .sort('price')
+   .select('name price');
    res.status(200).json({item})
 }
 
@@ -19,14 +21,31 @@ const getItem = async (req, res) => {
 
 const filterItems = async (req, res) => {
    const {name, price, category} = req.query
-   const itemQuery = {}
+   const itemQuery = {};
 
    if(name) {
-      itemQuery.name =  {$regrex: name, $otions: 'i'}
-   }
+      itemQuery.name = { $regex: name, $options: 'i' };
+   }                      
 
-   let result = await Item.find(itemQuery)
-   res.status(200).json({item})
+   let result =  Item.find(itemQuery)
+
+   if (sort) {
+      const sortList = sort.split(',').join(' ');
+      result = result.sort(sortList);
+    } else {
+      result = result.sort('createdAt');
+    }
+    
+    if (fields) {
+      const fieldsList = fields.split(',').join(' ');
+      result = result.select(fieldsList);
+    }
+
+    const items = await result;
+    res.status(200).json({items, nbHits: items.length})
+
+
+   res.status(200).json({result})
 }
 
 module.exports = {createItem, getItem,filterItems}
--- routes/item.js
+++ routes/item.js
@@ -10,5 +10,6 @@ const {createItem,getItem,filterItems} = require('../controllers/item')
 
 
 
-router.route('/').post(createItem).get(filterItems).get()
+router.route('/').post(createItem).get(filterItems)
+router.route('/static').get(getItem)
 module.exports = router;
